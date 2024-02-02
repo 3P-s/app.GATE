d55f895d1cc19fdb7a77ac9524493fbee029617d
viraj <viraj05082003patil@gmail.com>
2024-02-02 23:10:02 +0530

    updated question model

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import connectDB from './config/db.js';
+import connectDB from './db/configure.js';
 import dotenv from 'dotenv';
 dotenv.config();
-
 
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT = process.env.PORT;
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
